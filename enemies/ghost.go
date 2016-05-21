20c99b101080c22a385e402705adcf3bf815f894
Kalle Jillheden <kalle.jillheden@gmail.com>
2016-05-21 16:34:49 +0200

    renamed

--- enemies/ghost.go
+++ enemies/ghost.go
@@ -1,6 +1,6 @@
 components {
   id: "damage_script"
-  component: "/HelloLibrary/Scripts/DamageDealer.script"
+  component: "/oldHelloLibrary/Scripts/DamageDealer.script"
   position {
     x: 0.0
     y: 0.0
@@ -15,7 +15,7 @@ components {
 }
 components {
   id: "script"
-  component: "/HelloLibrary/Scripts/SimplePatrol.script"
+  component: "/oldHelloLibrary/Scripts/SimplePatrol.script"
   position {
     x: 0.0
     y: 0.0
@@ -30,7 +30,7 @@ components {
 }
 components {
   id: "script_collision"
-  component: "/HelloLibrary/Scripts/PlatformCollision.script"
+  component: "/oldHelloLibrary/Scripts/PlatformCollision.script"
   position {
     x: 0.0
     y: 0.0
@@ -46,36 +46,7 @@ components {
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
-  "mass: 0.0\n"
-  "friction: 0.1\n"
-  "restitution: 0.5\n"
-  "group: \"enemies\"\n"
-  "mask: \"hero\"\n"
-  "embedded_collision_shape {\n"
-  "  shapes {\n"
-  "    shape_type: TYPE_SPHERE\n"
-  "    position {\n"
-  "      x: 3.3234742\n"
-  "      y: 1.6617371\n"
-  "      z: 0.0\n"
-  "    }\n"
-  "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
-  "    }\n"
-  "    index: 0\n"
-  "    count: 1\n"
-  "  }\n"
-  "  data: 67.8727\n"
-  "}\n"
-  "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
-  ""
+  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"enemies\"\nmask: \"hero\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_SPHERE\n    position {\n      x: 3.3234742\n      y: 1.6617371\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 1\n  }\n  data: 67.8727\n}\nlinear_damping: 0.0\nangular_damping: 0.0\nlocked_rotation: false\n"
   position {
     x: 0.0
     y: 0.0
@@ -91,11 +62,7 @@ embedded_components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/enemies/enemies.atlas\"\n"
-  "default_animation: \"ghost\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
+  data: "tile_set: \"/enemies/enemies.atlas\"\ndefault_animation: \"ghost\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
   position {
     x: 0.0
     y: 0.0
--- enemies/spikes.go
+++ enemies/spikes.go
@@ -1,6 +1,6 @@
 components {
   id: "script"
-  component: "/HelloLibrary/Scripts/DamageDealer.script"
+  component: "/oldHelloLibrary/Scripts/DamageDealer.script"
   position {
     x: 0.0
     y: 0.0
@@ -16,38 +16,7 @@ components {
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
-  "mass: 0.0\n"
-  "friction: 0.1\n"
-  "restitution: 0.5\n"
-  "group: \"enemies\"\n"
-  "mask: \"hero\"\n"
-  "embedded_collision_shape {\n"
-  "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
-  "    position {\n"
-  "      x: -8.8301\n"
-  "      y: -6.622575\n"
-  "      z: 0.0\n"
-  "    }\n"
-  "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
-  "    }\n"
-  "    index: 0\n"
-  "    count: 3\n"
-  "  }\n"
-  "  data: 148.85287\n"
-  "  data: 76.556435\n"
-  "  data: 10.0\n"
-  "}\n"
-  "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
-  ""
+  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"enemies\"\nmask: \"hero\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: -8.8301\n      y: -6.622575\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 3\n  }\n  data: 148.85287\n  data: 76.556435\n  data: 10.0\n}\nlinear_damping: 0.0\nangular_damping: 0.0\nlocked_rotation: false\n"
   position {
     x: 0.0
     y: 0.0
@@ -63,11 +32,7 @@ embedded_components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/enemies/enemies.atlas\"\n"
-  "default_animation: \"spikes\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
+  data: "tile_set: \"/enemies/enemies.atlas\"\ndefault_animation: \"spikes\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
   position {
     x: 0.0
     y: 0.0
